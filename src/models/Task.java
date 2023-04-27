a216870c6bf6e9cfe28259319019b488a5714fd9
003.2023導入 <2023dounyukenshu.003@gmail.com>
2023-04-27 11:24:29 +0900

    getAllTasksを追加

--- src/models/Task.java
+++ src/models/Task.java
@@ -7,9 +7,22 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
 import javax.persistence.Table;
 
 @Entity
+@NamedQueries({
+    @NamedQuery(
+            name = "getAllTasks",
+            query = "SELECT t FROM Task AS t ORDER BY t.id DESC"
+            ),
+    @NamedQuery(
+            name = "getTasksCount",
+            query = "SELECT COUNT(t) FROM Task AS t"
+            )
+})
+
 @Table(name = "tasks")
 public class Task {
     @Id
